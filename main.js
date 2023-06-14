27b9da6ea59f8f7f837708f8ff6204ac95f8ad9d
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-06-13 22:35:20 -0500

    CV

---
+++ main.js
@@ -0,0 +1,365 @@
+let pages = {
+  contact: `<h2>Información de contacto</h2>
+  <br>
+  <div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Telefono: +57 313 3758491</h4>
+    <h4 class='card-title text-light'>Correo: <a href='mailto:nortigozab@udistrital.edu.co' class='text-light'>nortigozab@udistrital.edu.co</a></h4>
+    <h4 class='card-title text-light'><a href='https://www.linkedin.com/in/nicol%C3%A1s-ortigoza-berm%C3%BAdez-9020131a9/' target='_blank' rel='noopener noreferrer' class='text-light'>Linkedin</a></h4>
+
+  </div>
+</div>`,
+  education: `<h2>Formación</h2>
+  <br>
+  <div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Colegio CEDID Ciudad Bolivar (IED)</h4>
+    <div class="row">
+      <div class="col-md-6 text-light">
+        <h6>Bachiller académico</h6>
+        <h6>Mención de Honor, Mejor Bachiller Integral</h6>
+      </div>
+      <div class="col-md-6 text-light text-center">
+        <h5><strong>2014</strong></h5>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Universidad Distrital Francisco José de Caldas</h4>
+    <div class="row">
+      <div class="col-md-6 text-light">
+        <h6>Tecnologo en Sistematización de Datos</h6>
+      </div>
+      <div class="col-md-6 text-light text-center">
+        <h5><strong>2022</strong></h5>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Universidad Distrital Francisco José de Caldas</h4>
+    <div class="row">
+      <div class="col-md-6 text-light">
+        <h6>Ingeniería Telemática</h6>
+        <h6>9 Semestre</h6>
+      </div>
+      <div class="col-md-6 text-light text-center">
+        <h5><strong>Actualmente</strong></h5>
+      </div>
+    </div>
+  </div>
+</div>
+`,
+  experience: `<h2>Experiencia Laboral</h2>
+  <br>
+  <div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Sin Experiencia Laboral</h4>
+  </div>
+</div>
+<br>
+<h2>Voluntariados</h2>
+<br>
+<div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Universidad Distrital Francisco José de Caldas - Unidad de Extensión</h4>
+    <div class="row">
+      <div class="col-md-6 text-light">
+        <h6>Monitor</h6>
+        <h6>Herramientas Informáticas en la Nube</h6>
+      </div>
+      <div class="col-md-6 text-light text-center">
+        <h5><strong>2023</strong></h5>
+        <h6>40 Horas</h6>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<div class='card'>
+  <div class='card-body'>
+    <h4 class='card-title text-light'>Universidad Distrital Francisco José de Caldas  - Unidad de Extensión</h4>
+    <div class="row">
+      <div class="col-md-6 text-light">
+        <h6>Formador</h6>
+        <h6>Competencias digitales en Internet</h6>
+      </div>
+      <div class="col-md-6 text-light text-center">
+        <h5><strong>2022</strong></h5>
+        <h6>40 Horas</h6>
+      </div>
+    </div>
+  </div>
+</div>`,
+  knowledge: `<h2>Conocimiento</h2>
+  <br>
+  <div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">JavaScript</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">HTML</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">CSS</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Python</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Java</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">S.O. Linux</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Suite Office</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">MySql</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 65%;" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Git</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Aquí puedes agregar más columnas con tarjetas -->
+  
+  </div>
+
+</div>
+<h2>Habilidades Blandas</h2>
+  <br>
+  <div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Trabajo en Equipo</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Puntualidad</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Responsabilidad</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Trabajo bajo Presión</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Proactividad</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Creatividad</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Rapidez de Aprender</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Aquí puedes agregar más columnas con tarjetas -->
+  
+  </div>
+  
+</div>
+`,
+  tools: `<h2>Herramientas que domino</h2>
+  <br>
+  <div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Visual Studio Code</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">S.O. Linux</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">Microsft Office</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h6 class ="text-light">GitHub</h6>
+          <div class="progress">
+            <div class="progress-bar text-white bg-dark" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Aquí puedes agregar más columnas con tarjetas -->
+  
+  </div>
+
+</div>`,
+};
+
+function getPageContent(page) {
+  let contentToReturn;
+  switch (page) {
+    case "contact":
+      contentToReturn = pages.contact;
+      break;
+    case "education":
+      contentToReturn = pages.education;
+      break;
+    case "experience":
+      contentToReturn = pages.experience;
+      break;
+    case "knowledge":
+      contentToReturn = pages.knowledge;
+      break;
+    case "tools":
+      contentToReturn = pages.tools;
+      break;
+    default:
+      contentToReturn = pages.contact;
+      break;
+  }
+  document.getElementById("content").innerHTML = contentToReturn;
+
+  // Agregar clase 'active' al botón seleccionado
+  const navLinks = document.querySelectorAll(".nav-link");
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    selectedLink.classList.add("btn-outline-dark");
+  });
+  const selectedLink = document.querySelector(
+    `[onclick="getPageContent('${page}')"]`
+  );
+  selectedLink.classList.add("active");
+  link.classList.remove("btn-outline-dark");
+  link.classList.remove("btn-dark");
+}
